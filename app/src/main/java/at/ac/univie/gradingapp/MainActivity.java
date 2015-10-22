64f5d8392551d39a71f293de06beb8ae783e43fe
theendofdays <mlanger1986@yahoo.de>
2015-10-22 14:36:50 +0200

    main raus

--- app/src/main/java/at/ac/univie/gradingapp/MainActivity.java
+++ app/src/main/java/at/ac/univie/gradingapp/MainActivity.java
@@ -56,13 +56,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
